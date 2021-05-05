1773f2839f5d277c9011ba0ef3064ab1ef8f09dc
LAPTOP-JA5654I3\Liza <lizamihhedko@gmail.com>
2021-05-05 16:33:09 +0200

    Ejercicio 5

--- js/ejercicios.js
+++ js/ejercicios.js
@@ -1,12 +1,24 @@
-//Ejercicio 4
-
-let string = 'Estoy haciendo un ejercicio'
-
-let reverse = string => {
-    let arrayString = [...string]
-    let reverseString = arrayString.reverse()
-    let joinedString = reverseString.join('')
-    return joinedString
-}
-
-console.log(reverse(string))
+//Ejercicio 5
+function b() {
+    // tasques asíncrones , que triguen una estona..
+    setTimeout(() => {
+     return console.log('b')
+    }, 3000);
+    }
+     
+    async function a() {
+    // Ens esperem que la funció b acabi
+        try {
+            let result = await b();
+            doMoreWork()
+        }catch (error){
+            console.log(error)
+        }
+           
+    }
+    
+    function doMoreWork() {
+        return console.log('a');
+    }
+    
+    a()
