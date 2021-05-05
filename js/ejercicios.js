b0aa2d6f78e7b74fa66a9a9524c6b22dc6ccde18
LAPTOP-JA5654I3\Liza <lizamihhedko@gmail.com>
2021-05-05 16:47:15 +0200

    Ejercicio 8

--- js/ejercicios.js
+++ js/ejercicios.js
@@ -1,20 +1,60 @@
-//Ejercicio 7
+//Ejercicio 8
+
+window.onload = () => document.getElementById('button').addEventListener('click', makeTable);
+
 
 let makeTable = () => {
+    let number1 = parseInt(document.getElementById('number1').value)
+    let number2 = parseInt(document.getElementById('number2').value)
+    let span = document.getElementById('table')
+    span.innerHTML = '';
 
-    let number1 = prompt('Introduce número 1');
-    let number2 = prompt('Introduce número 2');
+    if (number1 > 10 && number2 > 10) {
+        span.innerHTML='Número 1 y 2 tienen que ser del 1 al 10';
 
-    if (number1 <= 10 && number2 <= 10) {
-        for(i = 1; i<=number2; i++) {
-            let result = number1 * i;
+    } else if (number2 > 10) {
+        span.innerHTML='Número 2 tiene que ser del 1 al 10';
 
-            console.log(`${number1} x ${i} = ${result}`); 
-        }
+    }else if (number1 > 10) {
+        span.innerHTML='Número 1 tiene que ser del 1 al 10';
+
+    }else if (isNaN(number1) || isNaN(number2)) {
+        span.innerHTML='Por favor, introduzca un número';
     }else {
-        console.log('Introduce un número del 1 al 10');
+
+        let table = document.createElement('table');
+        for(i = 1; i<= number2; i++) {
+            let tr = document.createElement('tr');   
+
+            let td1 = document.createElement('td');
+            let td2 = document.createElement('td');
+            let td3 = document.createElement('td');
+            let td4 = document.createElement('td');
+            let td5 = document.createElement('td');
+
+            let n1 = document.createTextNode(number1);
+            let x = document.createTextNode('x');
+            let n = document.createTextNode(i);
+            let total = document.createTextNode('=');
+            let result = document.createTextNode(number1 * i);
+
+            td1.appendChild(n1);
+            td2.appendChild(x);
+            td3.appendChild(n);
+            td4.appendChild(total);
+            td5.appendChild(result);
+
+            tr.appendChild(td1);
+            tr.appendChild(td2);
+            tr.appendChild(td3);
+            tr.appendChild(td4);
+            tr.appendChild(td5);
+
+            table.appendChild(tr);
+        }   
+        span.appendChild(table);  
     }
 
 }
 
-makeTable()
+makeTable();
