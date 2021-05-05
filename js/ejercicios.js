54ee9269252774cfb09431a28930d0e5509eaee5
LAPTOP-JA5654I3\Liza <lizamihhedko@gmail.com>
2021-05-05 16:30:35 +0200

    Ejercicio 2

--- js/ejercicios.js
+++ js/ejercicios.js
@@ -1,37 +1,13 @@
-//Ejercicio 1 
+//Ejercicio 2
 
-let multiply = (num1, num2) => num1 * num2;
+let users = 
+[{ firstName: 'Homer', lastName: 'Simpson' },
+{ firstName: 'Marge', lastName: 'Simpson' },
+{ firstName: 'Bart', lastName: 'Simpson' },
+{ firstName: 'Lisa', lastName: 'Simpson' },
+{ firstName: 'Maggie', lastName: 'Simpson' }];
 
-console.log(multiply(3,2))
+let userFirstName = users.map((user) => user.firstName);
 
-let toCelsius = fahrenheit => (5/9) * (fahrenheit-32);
+console.log(userFirstName)
 
-console.log(toCelsius(100))
- 
- // This function returns a string padded with leading zeros
-let padZeros = (num, totalLen) => {
-    let numStr = num.toString();
-    let numZeros = totalLen - numStr.length;
-    for (let i = 1; i <= numZeros; i++) {
-       numStr="0" + numStr; 
-    } 
-    return numStr;
-}
-
-console.log(padZeros(5,10))
- 
-let power = (base, exponent) => { 
-    let result=1; 
-    for (let i=0; i < exponent; i++) { 
-          result *=base; 
-    } 
-    return result;
-} 
- 
-console.log(power(5,2))
-
-let greet = who => console.log("Hello " + who);
-
-console.log(greet('Liza'))
-
-/////////////////////////////////////////////////////////////////////////////
